06bcb864fd7deb9cecdc7886962ce5e7e1a8df52
Asibul Hasan <asibulhasan.rucse.18@gmail.com>
2019-04-17 10:15:57 +0600

    Subtraction class created

---
+++ CalSubtraction.java
@@ -0,0 +1,8 @@
+public class CalSubtraction
+{
+	CalSubtraction(double firstValue,double secondValue)
+	{
+		double result = firstValue - secondValue ;
+		System.out.println("Subtraction result : " + result);
+	}
+}
